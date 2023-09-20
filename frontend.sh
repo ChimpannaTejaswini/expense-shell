09792c49dafa6916954c36e895c07dcfecddbb40
Ch.Tejaswini <tejaswinich94@gmail.com>
2023-09-20 09:51:19 +0530

    Expense script

--- frontend.sh
+++ frontend.sh
@@ -8,6 +8,7 @@ curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zi
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp expense.conf  /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf
+
 
 systemctl restart nginx
